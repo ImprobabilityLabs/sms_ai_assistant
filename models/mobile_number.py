22c7c013bfe858873a1fc0d6bda96d06347d35eb
Rahim Khoja <rahim@khoja.ca>
2024-05-11 10:28:33 -0700

    Create mobile_number.py

---
+++ models/mobile_number.py
@@ -0,0 +1,15 @@
+from .base import db, BaseModel
+from sqlalchemy.dialects.mysql import INTEGER  
+
+class MobileNumber(BaseModel):
+    __tablename__ = 'mobile_numbers'
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    subscription_id = db.Column(db.Integer, db.ForeignKey('subscriptions.id'), nullable=False)
+    country_code = db.Column(INTEGER(unsigned=True), nullable=False)
+    area_code = db.Column(INTEGER(unsigned=True), nullable=False)
+    mobile_number = db.Column(INTEGER(unsigned=True), nullable=False)
+
+    def __repr__(self):
+        full_number = f"+{self.country_code} ({self.area_code}) {self.mobile_number}"
+        return f"<MobileNumber {full_number}>"
