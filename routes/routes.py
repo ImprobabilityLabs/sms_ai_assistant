61c25f58132504e7c3f9b0298695282c408812bc
Rahim Khoja <rahim@khoja.ca>
2024-05-14 12:05:44 -0700

    Update routes.py

--- routes/routes.py
+++ routes/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, abort, current_app
 from oauthlib.oauth2 import WebApplicationClient
 from requests_oauthlib import OAuth2Session
-from models import User, Subscription, MobileNumber, History, UserPreference, AssistantPreference  # Import models
+from models import db, User, Subscription, MobileNumber, History, UserPreference, AssistantPreference  # Import models
 from utils.utility import fetch_data
 
 def configure_routes(app):
