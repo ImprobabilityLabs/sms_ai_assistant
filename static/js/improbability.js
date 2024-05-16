8d8a449ff38d5421aac892d80a3c34bb97e28932
Rahim Khoja <rahim@khoja.ca>
2024-05-15 22:23:58 -0700

    Update improbability.js

--- static/js/improbability.js
+++ static/js/improbability.js
@@ -194,20 +194,23 @@ window.addEventListener('resize', adjustFaqTitle);
             e.target.value = (month && year.length === 2) ? (month + '/' + year) : month + year;
           });
           
-              document.querySelectorAll('input[name="subscriptionOption"]').forEach(radio => {
-                  radio.addEventListener('change', function() {
-                      const details = document.querySelector('.purchase-details-price'); // Using class selector
-                      const detailstitle = document.querySelector('.purchase-details-price-title'); // Using class selector
-                      if (this.checked) {
-                          details.style.display = 'block';
-                          detailstitle.style.display = 'block';
-                          document.querySelector('.subscription-name').textContent = this.dataset.product;
-                          document.querySelector('.plan-final').textContent = `${this.dataset.cost} ${this.dataset.currency} per ${this.dataset.interval}`;
-                      } else {
-                          details.style.display = 'none'; // Optionally hide the details when another option is unchecked
-                      }
-                  });
-              });
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('input[name="subscriptionOption"]').forEach(radio => {
+        radio.addEventListener('change', function() {
+            const details = document.querySelector('.purchase-details-price'); // Using class selector
+            const detailstitle = document.querySelector('.purchase-details-price-title'); // Using class selector
+            if (this.checked) {
+                details.style.display = 'block';
+                detailstitle.style.display = 'block';
+                document.querySelector('.subscription-name').textContent = this.dataset.product;
+                document.querySelector('.plan-final').textContent = `${this.dataset.cost} ${this.dataset.currency} per ${this.dataset.interval}`;
+            } else {
+                details.style.display = 'none'; // Optionally hide the details when another option is unchecked
+            }
+        });
+    });
+});
+
           
           
             // Function to allow only numbers in the CVV field and limit to 4 characters
