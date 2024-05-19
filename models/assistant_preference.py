8b6657c89adc19e71c2d9a957b292605382247f7
Rahim Khoja <rahim@khoja.ca>
2024-05-19 14:07:14 -0700

    Update assistant_preference.py

--- models/assistant_preference.py
+++ models/assistant_preference.py
@@ -5,13 +5,13 @@ class AssistantPreference(BaseModel):
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     subscription_id = db.Column(db.Integer, db.ForeignKey('subscriptions.id'), nullable=False)
-    assistant_name = db.Column(db.String(32), nullable=False)
-    assistant_origin = db.Column(db.String(32), nullable=False)
-    assistant_gender = db.Column(db.String(16), nullable=False)
-    assistant_personality = db.Column(db.String(16), nullable=False)
-    assistant_response_style = db.Column(db.String(16), nullable=False)
-    assistant_demeanor = db.Column(db.String(16), nullable=False)
-    assistant_attitude = db.Column(db.String(16), nullable=False)
+    assistant_name = db.Column(db.String(64), nullable=False)
+    assistant_origin = db.Column(db.String(64), nullable=False)
+    assistant_gender = db.Column(db.String(64), nullable=False)
+    assistant_personality = db.Column(db.String(64), nullable=False)
+    assistant_response_style = db.Column(db.String(64), nullable=False)
+    assistant_demeanor = db.Column(db.String(64), nullable=False)
+    assistant_attitude = db.Column(db.String(64), nullable=False)
 
     def __repr__(self):
         return f"<AssistantPreference {self.assistant_name} for User {self.user_id}>"
