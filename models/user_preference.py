04aae055d26d40444361d5cadb2fe7c8194de9d3
Rahim Khoja <rahim@khoja.ca>
2024-05-19 04:10:25 -0700

    Update user_preference.py

--- models/user_preference.py
+++ models/user_preference.py
@@ -5,12 +5,12 @@ class UserPreference(BaseModel):
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     subscription_id = db.Column(db.Integer, db.ForeignKey('subscriptions.id'), nullable=False)
-    user_name = db.Column(db.String(32), nullable=False)
+    user_name = db.Column(db.String(64), nullable=False)
     user_title = db.Column(db.String(8), nullable=False)
-    user_measurement = db.Column(db.String(8), nullable=False)
+    user_measurement = db.Column(db.String(16), nullable=False)
     user_bio = db.Column(db.Text, nullable=True)
     user_language = db.Column(db.String(16), nullable=False)
-    user_location_full = db.Column(db.String(32), nullable=False)
+    user_location_full = db.Column(db.String(64), nullable=False)
     user_location_country = db.Column(db.String(3), nullable=True)
 
     def __repr__(self):
