d3157d411bafb434ef1e3b861c04b3611a276cde
Rahim Khoja <rahim@khoja.ca>
2024-05-20 19:57:00 -0700

    Update routes.py

--- routes/routes.py
+++ routes/routes.py
@@ -378,7 +378,7 @@ def configure_routes(app):
 
             current_app.logger.info(f'Assistant User Prompt: User ID={prompt}')
 
-            process_questions_answers(message_body, user_preferences.user_location_full, user_preferences.user_language)
+            process_questions_answers(message_body, user_preferences.user_location_full, user_preferences.user_location_country)
 
             if not user_id or not subscription_id:
                 return 'Unauthorized', 403
