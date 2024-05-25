b6ed9952078e5f4924f9512cbd499d612c9b6179
Rahim Khoja <rahim@khoja.ca>
2024-05-25 00:48:25 -0700

    Update routes.py

--- routes/routes.py
+++ routes/routes.py
@@ -226,7 +226,7 @@ def configure_routes(app):
                         'receipt_url': invoice.hosted_invoice_url  # URL for the printable receipt
                     })
 
-            return render_template('account.html', menu=menu, invoices=invoice_data)
+                return render_template('account.html', menu=menu, invoices=invoice_data)
         
             except Exception as e:
                 return jsonify({'error': str(e)}), 500
