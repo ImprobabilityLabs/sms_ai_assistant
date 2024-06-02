97776c8e6326302249d0986928e27f651c4b987c
Rahim Khoja <rahim@khoja.ca>
2024-06-02 10:14:42 -0700

    Create meta_tags.py

---
+++ meta_tags.py
@@ -0,0 +1,6 @@
+class MetaTags:
+    KEYWORDS = 'home, flask, main page'
+    DESCRIPTION = 'Welcome to the home page.'
+    LINKEDIN = 'about, company, information'
+    TWITTER = 'Learn more about us on this page.'
+    FACEBOOK = None
