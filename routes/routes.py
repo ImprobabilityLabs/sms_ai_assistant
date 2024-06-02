4198996eee94e9b2390a4998be83e12063ddecef
Rahim Khoja <rahim@khoja.ca>
2024-06-02 16:39:40 -0700

    Update routes.py

--- routes/routes.py
+++ routes/routes.py
@@ -138,85 +138,84 @@ def configure_routes(app):
                 current_app.logger.info('Received POST request with following form data:')
                 for key in request.form:
                     current_app.logger.info(f'{key}: {request.form[key]}')
-                    # Validate form data
-                    required_fields = [
-                        'subscriptionOption', 'assistant-name', 'assistant-origin', 'assistant-gender',
-                        'assistant-personality', 'assistant-response-style', 'user-name', 'user-location', 
-                        'user-mobile', 'user-language', 'user-title', 'user-measurement', 'user-description', 
-                        'card-name', 'billing-address', 'billing-country', 'billing-state', 'billing-zip', 
-                        'stripeToken'
-                    ]
+                # Validate form data
+                required_fields = [
+                    'subscriptionOption', 'assistant-name', 'assistant-origin', 'assistant-gender',
+                    'assistant-personality', 'assistant-response-style', 'user-name', 'user-location', 
+                    'user-mobile', 'user-language', 'user-title', 'user-measurement', 'user-description', 
+                    'card-name', 'billing-address', 'billing-country', 'billing-state', 'billing-zip', 
+                    'stripeToken'
+                ]
                     
-                    error_message = 'Please complete all required fields.'
-                    if all(field in request.form for field in required_fields):
-                        user = User.query.filter_by(provider_id=session.get('user_provider_id')).first()
-                        if user:
-                            referrer = session.get('referrer', '')
-                            success, error_message, subscription_id = handle_stripe_operations(user, request.form, referrer)
-                            current_app.logger.info("After handle_stripe_operations")
-                            if success:
-                                location_dict = get_location(request.form['user-location'])
-                                if location_dict['location_text'] != 'null':
-                                    location_user = location_dict['location_text']
-                                    location_country = location_dict['country_code']
-                                else:
-                                    location_user = request.form['user-location']
-                                    location_country = request.form['billing-country']
+                error_message = 'Please complete all required fields.'
+                if all(field in request.form for field in required_fields):
+                    user = User.query.filter_by(provider_id=session.get('user_provider_id')).first()
+                    if user:
+                        referrer = session.get('referrer', '')
+                        success, error_message, subscription_id = handle_stripe_operations(user, request.form, referrer)
+                        current_app.logger.info("After handle_stripe_operations")
+                        if success:
+                            location_dict = get_location(request.form['user-location'])
+                            if location_dict['location_text'] != 'null':
+                                location_user = location_dict['location_text']
+                                location_country = location_dict['country_code']
+                            else:
+                                location_user = request.form['user-location']
+                                location_country = request.form['billing-country']
                                 
-                                new_assistant_preference = AssistantPreference(
-                                    user_id=user.id,
-                                    subscription_id=subscription_id,
-                                    assistant_name=request.form['assistant-name'],
-                                    assistant_origin=request.form['assistant-origin'],
-                                    assistant_gender=request.form['assistant-gender'],
-                                    assistant_personality=request.form['assistant-personality'],
-                                    assistant_response_style=request.form['assistant-response-style']
-                                )
+                            new_assistant_preference = AssistantPreference(
+                                user_id=user.id,
+                                subscription_id=subscription_id,
+                                assistant_name=request.form['assistant-name'],
+                                assistant_origin=request.form['assistant-origin'],
+                                assistant_gender=request.form['assistant-gender'],
+                                assistant_personality=request.form['assistant-personality'],
+                                assistant_response_style=request.form['assistant-response-style']
+                            )
 
-                                db.session.add(new_assistant_preference)
-                                db.session.commit() 
+                            db.session.add(new_assistant_preference)
+                            db.session.commit() 
 
-                                new_user_preference = UserPreference(
-                                    user_id=user.id,
-                                    subscription_id=subscription_id,
-                                    user_name=request.form['user-name'],
-                                    user_title=request.form['user-title'],
-                                    user_measurement=request.form['user-measurement'],
-                                    user_bio=request.form.get('user-description', ''),
-                                    user_language=request.form['user-language'],
-                                    user_location_full=location_user, 
-                                    user_location_country=location_country
-                                )
+                            new_user_preference = UserPreference(
+                                user_id=user.id,
+                                subscription_id=subscription_id,
+                                user_name=request.form['user-name'],
+                                user_title=request.form['user-title'],
+                                user_measurement=request.form['user-measurement'],
+                                user_bio=request.form.get('user-description', ''),
+                                user_language=request.form['user-language'],
+                                user_location_full=location_user, 
+                                user_location_country=location_country
+                            )
 
-                                db.session.add(new_user_preference)
-                                db.session.commit()
+                            db.session.add(new_user_preference)
+                            db.session.commit()
                                 
-                                clean_number = clean_phone_number(request.form['user-mobile'])
-                                ctry_code = 1
+                            clean_number = clean_phone_number(request.form['user-mobile'])
+                            ctry_code = 1
 
-                                new_mobile_number = MobileNumber(
-                                    user_id=user.id,
-                                    subscription_id=subscription_id,
-                                    country_code=ctry_code,
-                                    mobile_number=int(clean_number)
-                                )
+                            new_mobile_number = MobileNumber(
+                                user_id=user.id,
+                                subscription_id=subscription_id,
+                                country_code=ctry_code,
+                                mobile_number=int(clean_number)
+                            )
     
-                                db.session.add(new_mobile_number)
-                                db.session.commit()
-                                
-                                subscription_rec = Subscription.query.filter_by(id=subscription_id, enabled=True).first()
+                            db.session.add(new_mobile_number)
+                            db.session.commit()
+                              
+                            subscription_rec = Subscription.query.filter_by(id=subscription_id, enabled=True).first()
 
-                                # Welcome Message 
-                                sys_prompt = build_system_prompt(new_user_preference, new_assistant_preference, extra_info=None, system_message = 'Create an initial introduction and welcome message for your user.')
-                                welcome_message = build_and_send_messages_openai(sys_prompt, history_records=None)
-                                send_reply(user.id, subscription_id, welcome_message, new_mobile_number.mobile_number, subscription_rec.twillio_number, Client(current_app.config['TWILIO_ACCOUNT_SID'], current_app.config['TWILIO_AUTH_TOKEN']), save_message=True)
+                            # Welcome Message 
+                            sys_prompt = build_system_prompt(new_user_preference, new_assistant_preference, extra_info=None, system_message = 'Create an initial introduction and welcome message for your user.')
+                            welcome_message = build_and_send_messages_openai(sys_prompt, history_records=None)
+                            send_reply(user.id, subscription_id, welcome_message, new_mobile_number.mobile_number, subscription_rec.twillio_number, Client(current_app.config['TWILIO_ACCOUNT_SID'], current_app.config['TWILIO_AUTH_TOKEN']), save_message=True)
 
-                                send_new_subscription_email(user.name, user.email, new_mobile_number.mobile_number, new_assistant_preference.assistant_name, subscription_rec.twillio_number)
+                            send_new_subscription_email(user.name, user.email, new_mobile_number.mobile_number, new_assistant_preference.assistant_name, subscription_rec.twillio_number)
                                 
-                                return redirect(url_for('dashboard_page'))
-                        else:
-                            current_app.logger.info("Before error_message")
-                            current_app.logger.error(error_message)
+                            return redirect(url_for('dashboard_page'))
+                    else:
+                        current_app.logger.error(error_message)
                         
             product_data = get_products()
             current_app.logger.info('Info: Subscribe Page - Products Object: ' + str(product_data))
