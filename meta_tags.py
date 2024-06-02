7c3f2b9ff92e748c76dda7255d44677dbb1dc1f0
Rahim Khoja <rahim@khoja.ca>
2024-06-02 10:15:13 -0700

    Update meta_tags.py

--- meta_tags.py
+++ meta_tags.py
@@ -4,3 +4,6 @@ class MetaTags:
     LINKEDIN = 'about, company, information'
     TWITTER = 'Learn more about us on this page.'
     FACEBOOK = None
+    GOOGLE_ANALYTICS_ID = 'G-3FLPEVT336'
+    GOOGLE_SITE_VERIFICATION = None
+    BING_SITE_VERIFICATION = None
