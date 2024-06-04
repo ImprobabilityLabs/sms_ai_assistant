c4ef9effecc5dfb671744cfae8a721d19968edf7
Rahim Khoja <rahim@khoja.ca>
2024-06-04 13:26:35 -0700

    Update routes.py

--- routes/routes.py
+++ routes/routes.py
@@ -54,6 +54,7 @@ def configure_routes(app):
             "Allow: /faq",
             f"Sitemap: {base_url}/sitemap.xml"
         ]
+        current_app.logger.info(f'Meta Tags Test: {str(MetaTags)}')
         return Response("\n".join(lines), mimetype="text/plain")
 
     @app.route('/', methods=['GET', 'POST'])
