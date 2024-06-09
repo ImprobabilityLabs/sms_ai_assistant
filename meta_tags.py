4bb075da7c6a99dfe2ab3805dbe4ba7ed6aefff6
Rahim Khoja <rahim@khoja.ca>
2024-06-09 10:19:02 -0700

    Update meta_tags.py

--- meta_tags.py
+++ meta_tags.py
@@ -1,5 +1,5 @@
 class MetaTags:
-    KEYWORDS = "SMS AI Assistant, Personal AI Companion, AI Assistance, Multilingual, Global AI Communication, Educational AI Assistant, Remote Area Connectivity, Customizable AI Settings, Environmentally Friendly AI, Dedicated SMS Number, SMS, Improbability Labs"
+    KEYWORDS = "AI SMS Chatbot, AI Assistant, What is AI assistance, Personal AI Assistant, AI Assistant App, SMS, SMS AI, AI Assisted Text Messaging, AI Text Messaging, Pocket AI, SMS Chatbot, SMS AI Chatbot, AI Text Message Assistant, Custom AI assistant, Multilingual AI, No Data AI, Improbability Labs, Improbability"
     DESCRIPTION = "Your pocket AI companion - Explore, learn, decide—anywhere, anytime with your SMS AI Assistant. No internet? No problem! Personalized AI at your fingertips."
     LINKEDIN = "https://www.linkedin.com/company/improbability-labs/"
     TWITTER = "https://twitter.com/ImprobLabs"
